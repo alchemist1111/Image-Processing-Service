e1a2c6db491c6383c8b493305caf7ffd7f8116d2
alchemist1111 <emoyoeugine@gmail.com>
2025-12-24 15:35:03 +0300

    Add user registration and login url paths

---
+++ backend/users/urls.py
@@ -0,0 +1,19 @@
+from django.urls import path
+from .views import UserRegistrationView, UserLoginView
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
+
+
+urlpatterns = [
+    # JWT token obtain url
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    
+    # User registration url
+    path('register/', UserRegistrationView.as_view(), name='user-registration'),
+    
+    # User login url
+    path('login/', UserLoginView.as_view(), name='user-login'),
+]
