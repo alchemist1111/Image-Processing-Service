d4bc5ff9476f70e4a1bf87be64155b153393392e
alchemist1111 <emoyoeugine@gmail.com>
2025-12-28 05:31:28 +0300

    Add views for listing images and image upload

--- backend/image_management/views.py
+++ backend/image_management/views.py
@@ -1,3 +1,35 @@
-from django.shortcuts import render
+from rest_framework import viewsets, status
+from .serializers import ImageSerializer, ImageUploadSerializer
+from rest_framework.response import Response
+from rest_framework import permissions
+from .models import Image
+from rest_framework.parsers import MultiPartParser, FormParser
 
-# Create your views here.
+# Image viewset
+class ImageViewSet(viewsets.ModelViewSet):
+    """
+       ViewSet for viewing and editing Image instances.
+       
+    """
+    queryset = Image.objects.all()
+    serializer_class = ImageSerializer
+    permission_classes = [permissions.AllowAny]
+    
+
+# Image upload viewset
+class ImageUploadViewSet(viewsets.ModelViewSet):
+    """
+       ViewSet for handling image upload functionality.
+       
+    """
+    queryset = Image.objects.all()
+    serializer_class = ImageUploadSerializer
+    # permission_classes = [permissions.IsAuthenticated]
+    permission_classes = [permissions.AllowAny]
+    parser_classes = (MultiPartParser, FormParser)
+    
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        self.perform_create(serializer)
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
